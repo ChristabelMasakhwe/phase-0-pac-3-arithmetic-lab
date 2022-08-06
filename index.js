46f9375705dd3fb3770c75be6247567f983bb681
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-08-06 20:16:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+var a, b
+function add(a, b) {
+    return(a + b);
+}
+function subtract(a, b) {
+    return(a - b);
+}
+function multiply(a, b) {
+    return(a * b);
+}
+function divide(a, b) {
+    return(a / b);
+}
+function increment(a) {
+    return(a + 1);
+}
+function decrement(a) {
+    return(a - 1);
+}
+function makeInt(a) {
+    parseInt(a, 10); {
+        return(a);
+    }
+}
+function makeInt(n) {
+    let number = parseInt(n, 10);
+    return number
+}
+function preserveDecimal(n) {
+    let float = parseFloat(n);
+    return float
+    }
